b5650fdf070dfb11a83483fde00c3efda1cfa41c
mubarek78 <shifamubarek@yahoo.com>
2022-03-26 20:16:23 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin (record) {
+    const result = record.find(object => object.result === 'W');
+    if (result) {
+        return result.year
+    }
+  
+}
